8193194538002cc44abeff9482258212138b734a
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-09-09 16:54:14 -0600

    ensure search only applies when lenght is greater than zero

--- src/components/Search-Games.tsx
+++ src/components/Search-Games.tsx
@@ -14,6 +14,7 @@ export function SearchGames({ setGames }: SearchGamesProps) {
 			const res = await SpeedRunApiService.fetchGamesByName(name);
 			setGames(res);
 		}
+        if(!searchTerm) return
 		searchGame(searchTerm);
 	}, [searchTerm]);
     return (
